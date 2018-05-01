e23d7cfd83e4e929a2af8a7d71317a4ad5e981e4
joconnell714 <jjo315@nyu.edu>
2018-05-01 02:28:26 -0400

    Added Drawing 2
    Added webpage, stylesheet, and scripts for drawing 2. Added background image, svg file, and .wav files for drawing 2. Updated index page to link to drawing 2.

---
+++ drawing2.js
@@ -0,0 +1,13 @@
+document.getElementById('myAudio').addEventListener('ended',repeat,false);
+function repeat(e) {
+    setTimeout(function(){
+        document.getElementById('myAudio').play();
+    }, 6000);
+}
+
+document.getElementById('myAudio2').addEventListener('ended',repeat2,false);
+function repeat2(e) {
+    setTimeout(function(){
+        document.getElementById('myAudio').play();
+    }, 4500);
+}
